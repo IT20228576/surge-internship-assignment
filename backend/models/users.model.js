8065d9eb12cd1e7ec01ea696c5528a8ba74f7529
Chirath Jayawardena <88072810+IT20228576@users.noreply.github.com>
2022-08-08 01:34:06 +0530

    add email verification and update user model

--- backend/models/users.model.js
+++ backend/models/users.model.js
@@ -3,11 +3,11 @@ const mongoose = require("mongoose");
 const usersSchema = new mongoose.Schema(
   {
     id: { type: Number, required: true },
-    firstName: { type: String, required: true },
-    lastName: { type: String, required: true },
-    email: { type: String, required: true },
-    dateOfBirth: { type: Date, required: true },
-    mobile: { type: Number, required: true },
+    firstName: { type: String },
+    lastName: { type: String },
+    email: { type: String, required: true, unique: true },
+    dateOfBirth: { type: Date },
+    mobile: { type: Number },
     status: { type: Boolean, default: false },
     password: { type: String, required: true },
     accountType: { type: String, required: true },
--- backend/routes/users.routes.js
+++ backend/routes/users.routes.js
@@ -1,20 +1,17 @@
 const router = require("express").Router();
 const Users = require("../models/users.model");
+const crypto = require("crypto");
+const Email = require("../utils/email.util");
 
 /* The above code is a route handler for the /register route. It is used to register a new User. */
 router.post("/create", async (req, res) => {
   try {
     /* Destructuring the request body. */
-    const {
-      id,
-      firstName,
-      lastName,
-      email,
-      dateOfBirth,
-      mobile,
-      password,
-      accountType,
-    } = req.body;
+    const { firstName, lastName, email, dateOfBirth, mobile, accountType } =
+      req.body;
+
+    const id = Math.random().toString(8).substring(5, 15);
+    const oneTimePassword = crypto.randomBytes(10).toString("hex");
 
     // save a new user account to the db
     const newUser = new Users({
@@ -24,12 +21,15 @@ router.post("/create", async (req, res) => {
       email: email,
       dateOfBirth: dateOfBirth,
       mobile: mobile,
-      password: password,
+      password: oneTimePassword,
       accountType: accountType,
     });
 
     /* Saving the new user to the database. */
-    await newUser.save();
+    const savedUser =  await newUser.save();
+
+    /* Sending an verification email to the user. */
+    await Email.sendVerification(savedUser.email, oneTimePassword);
 
     /* Sending a response to the client. */
     res.status(201).send({ Message: "Successfully created new user" });
