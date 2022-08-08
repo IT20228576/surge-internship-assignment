084682361f1c6e838eca9e9b1903281f5819ff12
Chirath Jayawardena <88072810+IT20228576@users.noreply.github.com>
2022-08-09 00:14:09 +0530

    add basic frontend configuration

---
+++ frontend/src/App.jsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import axios from "axios";
+
+import Home from "./components/main/Home.component";
+import Login from "./components/main/Login.component";
+import Navbar from "./components/main/NavBar.components";
+import Register from "./components/main/Register.component";
+
+axios.defaults.withCredentials = true;
+
+const App = () => {
+  const [user, setUser] = useState("");
+  const [status, setStatus] = useState("");
+
+  useEffect(() => {
+    setUser(localStorage.getItem("type"));
+    setStatus(localStorage.getItem("status"));
+  }, []);
+  
+  return (
+    (
+      <div>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route
+              exact
+              path="/"
+              element={user !== null ? <Home /> : <Login />}
+            />
+            <Route
+              exact
+              path="/register"
+              element={status === false ? <Login /> : <Register />}
+            />
+          </Routes>
+        </Router>
+      </div>
+    )
+  );
+};
+
+export default App;
